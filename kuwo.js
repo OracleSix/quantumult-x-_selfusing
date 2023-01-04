6abe1962fb7aea1ebccfa26d3c719f6c30950155
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-01-04 22:51:27 +0800

    Update kuwo.js

--- kuwo.js
+++ kuwo.js
@@ -3,8 +3,9 @@ var obj = JSON.parse(body);
 
 //obj.vipAutoPayUser = 1 ;
 //obj.isNewUser = 0 ;
-var data = obj.data
-data.isNewUser = "0"
+var data = obj.data ;
+data.isNewUser = "0" ;
+data.vipAutoPayUser = "1" ;
 
 body = JSON.stringify(obj);
 $done(body);
