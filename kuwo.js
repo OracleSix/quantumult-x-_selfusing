90829cd7bd0e6d3229eabd1d22f57abe0f91a090
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-01-05 11:57:31 +0800

    Update kuwo.js

--- kuwo.js
+++ kuwo.js
@@ -3,17 +3,17 @@ var obj = JSON.parse(body);
 
 //obj.vipAutoPayUser = 1 ;
 //obj.isNewUser = 0 ;
-var data = obj.data ;
+//var data = obj.data ;
 data.isNewUser =0;
 //data.vipmAutoPayUser = "1" ;
 //data.svipAutoPayUser = "1" ;
-data.vipOverSeasExpire=3031202489;
-data.vipmExpire=3031202489;
-data.vip3Expire=3031202489;
-data.experienceExpire=3031202489;
-data.vipLuxuryExpire=3031202489;
-data.vipExpire=3031202489;
-data.svipExpire=3031202489;
+obj.data.vipOverSeasExpire=3031202489;
+obj.data.vipmExpire=3031202489;
+obj.data.vip3Expire=3031202489;
+obj.data.experienceExpire=3031202489;
+obj.data.vipLuxuryExpire=3031202489;
+obj.data.vipExpire=3031202489;
+obj.data.svipExpire=3031202489;
 
 body = JSON.stringify(obj);
 $done(body);
